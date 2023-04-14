6ae5cbadd09cf852469c58b5eb27956a8504c7ed
weiting <kingkwek520@gmail.com>
2023-04-14 17:49:32 +0800

    remove sudo no need

--- neo4j-setup.sh
+++ neo4j-setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo amazon-linux-extras install epel -y
-sudo yum install -y java-11-amazon-corretto-headless neo4j-enterprise-5.6.0-1.noarch
+yum update -y
+amazon-linux-extras install epel -y
+yum install -y java-11-amazon-corretto-headless neo4j-enterprise-5.6.0-1.noarch
 
-sudo sed -i 's/#dbms.default_listen_address=0.0.0.0/dbms.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
-sudo sed -i 's/#server.default_listen_address=0.0.0.0/server.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
+sed -i 's/#dbms.default_listen_address=0.0.0.0/dbms.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
+sed -i 's/#server.default_listen_address=0.0.0.0/server.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
 
-sudo sed -i 's/#server.http.enabled=true/server.http.enabled=true/g' /etc/neo4j/neo4j.conf
-sudo sed -i 's/#server.http.listen_address=0.0.0.0:7474/server.http.listen_address=:7474/g' /etc/neo4j/neo4j.conf
-sudo sed -i 's/#server.http.advertised_address=/server.http.advertised_address=:7474/g' /etc/neo4j/neo4j.conf
+sed -i 's/#server.http.enabled=true/server.http.enabled=true/g' /etc/neo4j/neo4j.conf
+sed -i 's/#server.http.listen_address=0.0.0.0:7474/server.http.listen_address=:7474/g' /etc/neo4j/neo4j.conf
+sed -i 's/#server.http.advertised_address=/server.http.advertised_address=:7474/g' /etc/neo4j/neo4j.conf
 
-sudo sed -i 's/#dbms.connector.bolt.enabled=true/dbms.connector.bolt.enabled=true/g' /etc/neo4j/neo4j.conf
-sudo sed -i 's/#dbms.connector.bolt.tls_level=DISABLED/dbms.connector.bolt.tls_level=DISABLED/g' /etc/neo4j/neo4j.conf
-sudo sed -i 's/#dbms.connector.bolt.listen_address=:7687/dbms.connector.bolt.listen_address=:7687/g' /etc/neo4j/neo4j.conf
-sudo sed -i 's/#dbms.connector.bolt.advertised_address=:7687/dbms.connector.bolt.advertised_address=:7687/g' /etc/neo4j/neo4j.conf
+sed -i 's/#dbms.connector.bolt.enabled=true/dbms.connector.bolt.enabled=true/g' /etc/neo4j/neo4j.conf
+sed -i 's/#dbms.connector.bolt.tls_level=DISABLED/dbms.connector.bolt.tls_level=DISABLED/g' /etc/neo4j/neo4j.conf
+sed -i 's/#dbms.connector.bolt.listen_address=:7687/dbms.connector.bolt.listen_address=:7687/g' /etc/neo4j/neo4j.conf
+sed -i 's/#dbms.connector.bolt.advertised_address=:7687/dbms.connector.bolt.advertised_address=:7687/g' /etc/neo4j/neo4j.conf
 
-sudo sed -i 's/#dbms.logs.http.enabled=true/dbms.logs.http.enabled=true/g' /etc/neo4j/neo4j.conf
-sudo sed -i 's/#dbms.security.allow_csv_import_from_file_urls=true/dbms.security.allow_csv_import_from_file_urls=true/g' /etc/neo4j/neo4j.conf
+sed -i 's/#dbms.logs.http.enabled=true/dbms.logs.http.enabled=true/g' /etc/neo4j/neo4j.conf
+sed -i 's/#dbms.security.allow_csv_import_from_file_urls=true/dbms.security.allow_csv_import_from_file_urls=true/g' /etc/neo4j/neo4j.conf
 
-sudo systemctl start neo4j
+systemctl start neo4j
