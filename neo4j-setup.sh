6fb1d7784105121d6ad7cc1f628e8344c437c367
weiting <kingkwek520@gmail.com>
2023-04-14 18:13:38 +0800

    add liscence agreement and add  logging

--- neo4j-setup.sh
+++ neo4j-setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-yum update -y
-amazon-linux-extras install epel -y
-yum install -y java-11-amazon-corretto-headless neo4j-enterprise-5.6.0-1.noarch
+LOG_FILE="/tmp/neo4jsetup.log" 
+
+export NEO4J_ACCEPT_LICENSE_AGREEMENT=eval
+
+echo "Start running" >> $LOG_FILE
+
+yum update -y >> $LOG_FILE
+
+yum install -y java-11-amazon-corretto-headless >> $LOG_FILE
+
+yum install -y neo4j-enterprise-5.6.0-1.noarch >> $LOG_FILE
 
 sed -i 's/#dbms.default_listen_address=0.0.0.0/dbms.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
 sed -i 's/#server.default_listen_address=0.0.0.0/server.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
@@ -19,4 +27,6 @@ sed -i 's/#dbms.connector.bolt.advertised_address=:7687/dbms.connector.bolt.adve
 sed -i 's/#dbms.logs.http.enabled=true/dbms.logs.http.enabled=true/g' /etc/neo4j/neo4j.conf
 sed -i 's/#dbms.security.allow_csv_import_from_file_urls=true/dbms.security.allow_csv_import_from_file_urls=true/g' /etc/neo4j/neo4j.conf
 
-systemctl start neo4j
+systemctl start neo4j >> $LOG_FILE
+
+echo "Script completed successfully" >> $LOG_FILE
