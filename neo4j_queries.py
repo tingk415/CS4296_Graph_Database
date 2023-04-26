683c7510b79122b00c6eec351327c8db5ffcf89d
weiting <kingkwek520@gmail.com>
2023-04-26 22:08:15 +0800

    working code

--- neo4j_queries.py
+++ neo4j_queries.py
@@ -8,8 +8,12 @@ URI = "neo4j://"+EC2_PUBLIC_IP+":7687"
 #AUTH = ("neo4j", "CS4296NEO4J")
 AUTH = ("neo4j", "reactome")
 
+TIME_RECORDS=[]
+
+QUERY_TYPE=['Node and relationship traversal','Pathfinding','Graph analytics','Text search','Data modification']
+
 QUERIES=[
-    #Node and relationship traversal
+    #Node and relationship traversal 0
     """
     MATCH (n)
     WITH n, RAND() AS rand
@@ -21,46 +25,55 @@ QUERIES=[
     WHERE ID(m) = nodeId
     RETURN nodeId, count(r2) AS secondhopRelationshipCount
     """,
-    #Pathfinding
+    #Pathfinding 1
     """
     MATCH (n1),(n2)
     WHERE n1.dbId = 1226097 AND n2.dbId = 9626034
     MATCH p = shortestPath((n1)-[*]-(n2))
     RETURN p
     """,
-    #Graph analytics
+    #Graph analytics 2
     """
-    CALL algo.pageRank('Node', 'INTERACTS_WITH', {iterations: 20})
+    CALL gds.graph.project(
+    'myGraph',
+    'DatabaseObject',
+    'species'
+    )
+    YIELD graphName, nodeCount, relationshipCount
+    RETURN graphName, nodeCount, relationshipCount
+    """,
+    #pagerank
+    """
+    CALL gds.pageRank.stream('myGraph')
     YIELD nodeId, score
-    RETURN algo.getNodeById(nodeId).displayName AS name, score
-    ORDER BY score DESC
-    LIMIT 10
+    RETURN gds.util.asNode(nodeId).name AS name, score
+    ORDER BY score DESC, name ASC
+    """,
+    #degree centrality
+    """
+    CALL gds.degree.stream('myGraph')
+    YIELD nodeId, score
+    RETURN gds.util.asNode(nodeId).name AS name, score AS degrees
+    ORDER BY degrees DESC, name DESC
+    """,
+    #Text search 3
+    """
+    CREATE FULLTEXT INDEX dpname FOR (n:DatabaseObject) ON EACH[n.displayName]
     """,
     """
-    CALL algo.betweenness('Node', 'INTERACTS_WITH', {direction: 'both'})
-    YIELD nodeId, centrality
-    RETURN algo.getNodeById(nodeId).displayName AS name, centrality
-    ORDER BY centrality DESC
-    LIMIT 10
-    """,
-    #Data modification
-    """
-    CREATE (n:Node {displayName: $name, species: $species})
-    """,
-    """
-    MATCH (n)-[r:INTERACTS_WITH]->(m)
-    WHERE id(n) = $nodeId
-    DELETE r
-    """,
-    #Text search
-    """
-    CALL db.index.fulltext.queryNodes('displayName', $keyword)
+    CALL db.index.fulltext.queryNodes('dpname', 'Ontario')
     YIELD node
-    RETURN node.displayName, node.species
+    RETURN node.displayName
+    """,
+    #Data modification 4
     """
+    MATCH (n)
+    WHERE ID(n) = 1
+    SET n.hasDiagram = true
+    RETURN n
+    """,
 
 ]
-
 
 def time_query(session,query):
     start_time = time.time()
@@ -68,6 +81,7 @@ def time_query(session,query):
     end_time = time.time()
     elapsed_time = end_time - start_time
     print("Query took {} seconds".format(elapsed_time))
+    TIME_RECORDS.append(elapsed_time)
     return results
 
 
@@ -76,3 +90,12 @@ with GraphDatabase.driver('bolt://localhost:7687', auth=AUTH) as driver:
     with driver.session(database="neo4j") as session:
         for query in QUERIES:
             time_query(session,query)
+
+    del TIME_RECORDS[2]
+    del TIME_RECORDS[4]
+
+    TIME_RECORDS[3] += TIME_RECORDS.pop(2)
+
+    for (idx, i) in enumerate(TIME_RECORDS):
+        time_str = "{:.3f}".format(i)
+        print(QUERY_TYPE[idx]+':'+time_str)
